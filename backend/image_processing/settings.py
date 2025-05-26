b0a5a4ee9ac6d573f042d3083cbe3b13404e317a
zhaoicat <105899153@qq.com>
2025-05-26 14:38:50 +0800

    s

--- backend/image_processing/settings.py
+++ backend/image_processing/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-=!q0cm&&vv6#(@ynfdk!wk^g%3*cjmdk7q=+l6v5l50mnmq4o5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // 从环境变量获取后端地址，默认为localhost:8000
-const BACKEND_URL = process.env.VITE_BACKEND_HOST || 'http://localhost:8000'
+const BACKEND_URL = process.env.VITE_BACKEND_HOST || 'http://localhost:8888'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: BACKEND_URL,
